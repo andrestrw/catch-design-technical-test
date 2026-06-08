d9871bda5cd4f03bafe782c30e80075683d4d1b6
andrestrw <133chicagobulls@gmail.com>
2026-06-08 15:14:55 +1200

    style(layout): add responsive margins and GitHub dark background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getRepositories } from "../application/get-repositories";
 import { RepoList } from "../presentation/components/RepoList/RepoList";
 import { Pagination } from "../presentation/components/Pagination/Pagination";
+import styles from "./page.module.css";
 
 interface HomeProps {
     searchParams: Promise<{ page?: string }>;
@@ -13,8 +14,8 @@ export default async function Home({ searchParams }: HomeProps) {
     const { repos, hasNextPage } = await getRepositories(currentPage);
 
     return (
-        <main>
-            <h1>GitHub Repositories</h1>
+        <main className={styles.main}>
+            <h1 className={styles.title}>GitHub Repositories</h1>
             <RepoList repos={repos} />
             <Pagination currentPage={currentPage} hasNextPage={hasNextPage} />
         </main>
